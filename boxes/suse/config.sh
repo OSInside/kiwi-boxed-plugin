fafb83425d5066d5a32c31123c230f7b0371f781
Marcus Schäfer <ms@suse.de>
2020-04-06 12:22:26 +0200

    Added suse box image description

---
+++ boxes/suse/config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
+
+#==================================
+# Disable services
+#----------------------------------
+systemctl mask systemd-logind.service
+systemctl mask systemd-update-utmp.service
+systemctl mask auditd.service
+systemctl mask systemd-update-utmp-runlevel.service
+systemctl mask systemd-user-sessions.service
+
+#=====================================
+# Enable Shared Description directory
+#-------------------------------------
+mkdir -p /description
+systemctl enable description.automount
+
+#==================================
+# Enable Shared Bundle Directory
+#----------------------------------
+mkdir -p /bundle
+systemctl enable bundle.automount
+
+#======================================
+# Activate kiwi service
+#--------------------------------------
+suseInsertService kiwi

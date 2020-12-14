3afb6dc4491e9ff007b2cf5feef08de87eef140e
Marcus Schäfer <ms@suse.de>
2020-12-14 12:00:00 +0100

    Bump version: 0.1.14 → 0.1.15

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.14'
+version = '0.1.15'
 # The full version, including alpha/beta/rc tags.
 release = version
 

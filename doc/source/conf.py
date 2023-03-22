e1a77fbcf7699aba28b8a9f67e7a6030846f9414
Marcus Schäfer <marcus.schaefer@gmail.com>
2023-03-22 10:09:13 +0100

    Bump version: 0.2.24 → 0.2.25

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ author = 'Marcus Schäfer'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.24'
+version = '0.2.25'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- kiwi_boxed_plugin/version.py
+++ kiwi_boxed_plugin/version.py
@@ -18,5 +18,5 @@
 """
 Global version information used in kiwi-boxed-build and the package
 """
-__version__ = '0.2.24'
+__version__ = '0.2.25'
 __githash__ = '$Format:%H$'

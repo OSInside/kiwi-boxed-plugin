96da20ed6a5ebad41393924fa5295b1438b686ce
Bo Maryniuk <bo@maryniuk.net>
2023-10-24 16:48:24 +0200

    Add unit test test_get_plugin_config_file_env

--- test/unit/defaults_test.py
+++ test/unit/defaults_test.py
@@ -1,6 +1,7 @@
 from pkg_resources import resource_filename
-
 from kiwi_boxed_plugin.defaults import Defaults
+from mock import patch, Mock
+import os
 
 
 class TestDefaults:
@@ -8,3 +9,9 @@ class TestDefaults:
         assert Defaults.get_plugin_config_file() == resource_filename(
             'kiwi_boxed_plugin', 'config/kiwi_boxed_plugin.yml'
         )
+
+    @patch("os.path.exists", lambda f: True)
+    @patch.dict(os.environ, KIWI_BOXED_PLUGIN_CFG="aarchderwelt.conf")
+    def test_get_plugin_config_file_env(self):
+        assert Defaults.get_plugin_config_file() == "aarchderwelt.conf", \
+            "aarch64 aonfiguration from the environment variable do not match"

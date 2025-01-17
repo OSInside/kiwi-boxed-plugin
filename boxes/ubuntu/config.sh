8303d937656a5fdb5ad2b6981d05864a7854fda5
Isaac True <isaac@is.having.coffee>
2025-01-17 14:00:29 +0100

    boxes: ubuntu: remove service to set the resolv.conf symlink
    This step is performed automatically by the installation process of
    systemd-resolved.

--- boxes/ubuntu/config.sh
+++ boxes/ubuntu/config.sh
@@ -34,11 +34,6 @@ systemctl disable lvm2-lvmetad.socket
 systemctl mask lvm2-lvmetad.socket
 
 #======================================
-# Enable firstboot resolv.conf setting
-#--------------------------------------
-baseInsertService symlink-resolvconf
-
-#======================================
 # Enable systemd networking services
 #--------------------------------------
 baseInsertService systemd-networkd
